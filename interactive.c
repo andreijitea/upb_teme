635191c288f620537201e93292c85c2baabe4d19
Andrei Jitea <jitea_andrei@outlook.com>
2023-12-10 14:32:47 +0200

    Finalizare tema

--- interactive.c
+++ interactive.c
@@ -4,6 +4,10 @@
 #include "imageprocessing.h"
 #include "bmp.h"
 
+#define SIZE_ARRAY 20
+#define SIZE_PATH 50
+#define SIZE_COMMAND 2
+
 typedef struct {
     int ***matrice;
     int N, M;
@@ -14,13 +18,105 @@ typedef struct {
     int size;
 } filtru;
 
+int ***load_image(int N, int M, char *path);
+void delete_image(imagine imagini[], int index, int i_img);
+float **load_filter(int size);
+void delete_filter(filtru filtre[SIZE_ARRAY], int index, int i_fil);
+void save(int index, imagine imagini[], int N, int M, char *path);
+void delete_all(imagine imagini[], int i_img, filtru filtre[], int i_fil);
+
+int main() {
+    int i_img = 0, i_fil = 0;
+    imagine imagini[SIZE_ARRAY];
+    filtru filtre[SIZE_ARRAY];
+    char comanda[SIZE_COMMAND];
+
+    do {
+        scanf("%s", comanda);
+
+        if (strcmp(comanda, "l") == 0) {
+            int N = 0, M = 0;
+            char path[SIZE_PATH];
+            scanf("%d %d %s", &N, &M, path);
+            imagini[i_img].matrice = load_image(N, M, path);
+            imagini[i_img].N = N;
+            imagini[i_img].M = M;
+            i_img++;
+        } else if (strcmp(comanda, "s") == 0) {
+            int index = 0;
+            char path[SIZE_PATH];
+            scanf("%d %s", &index, path);
+            save(index, imagini, imagini[index].N, imagini[index].M, path);
+        } else if (strcmp(comanda, "ah") == 0) {
+            int index = 0;
+            scanf("%d", &index);
+            imagini[index].matrice = flip_horizontal(imagini[index].matrice, imagini[index].N, imagini[index].M);
+        } else if (strcmp(comanda, "ar") == 0) {
+            int index = 0, temp = 0;
+            scanf("%d", &index);
+            imagini[index].matrice = rotate_left(imagini[index].matrice, imagini[index].N, imagini[index].M);
+            temp = imagini[index].N;
+            imagini[index].N = imagini[index].M;
+            imagini[index].M = temp;
+        } else if (strcmp(comanda, "ac") == 0) {
+            int index = 0, x = 0, y = 0, h = 0, w = 0;
+            scanf("%d %d %d %d %d", &index, &x, &y, &w, &h);
+            imagini[index].matrice = crop(imagini[index].matrice, imagini[index].N, imagini[index].M, x, y, h, w);
+            imagini[index].N = h;
+            imagini[index].M = w;
+        } else if (strcmp(comanda, "ae") == 0) {
+            int index = 0, rows = 0, cols = 0, R = 0, G = 0, B = 0;
+            scanf("%d %d %d %d %d %d", &index, &rows, &cols, &R, &G, &B);
+            imagini[index].matrice = extend(imagini[index].matrice, imagini[index].N, imagini[index].M,
+                                            rows, cols, R, G, B);
+            imagini[index].N += 2 * rows;
+            imagini[index].M += 2 * cols;
+        } else if (strcmp(comanda, "ap") == 0) {
+            int index_dst = 0, index_src = 0, x = 0, y = 0;
+            scanf("%d %d %d %d", &index_dst, &index_src, &x, &y);
+            imagini[index_dst].matrice = paste(imagini[index_dst].matrice, imagini[index_dst].N, imagini[index_dst].M,
+                                               imagini[index_src].matrice, imagini[index_src].N, imagini[index_src].M,
+                                               x, y);
+        } else if (strcmp(comanda, "cf") == 0) {
+            int size = 0;
+            scanf("%d", &size);
+            filtre[i_fil].matrice = load_filter(size);
+            filtre[i_fil].size = size;
+            i_fil++;
+        } else if (strcmp(comanda, "af") == 0) {
+            int index_img = 0, index_filter = 0;
+            scanf("%d %d", &index_img, &index_filter);
+            imagini[index_img].matrice = apply_filter(imagini[index_img].matrice, imagini[index_img].N,
+                                                      imagini[index_img].M, filtre[index_filter].matrice,
+                                                      filtre[index_filter].size);
+        } else if (strcmp(comanda, "df") == 0) {
+            int index = 0;
+            scanf("%d", &index);
+            delete_filter(filtre, index, i_fil);
+            i_fil--;
+        } else if (strcmp(comanda, "di") == 0) {
+            int index = 0;
+            scanf("%d", &index);
+            delete_image(imagini, index, i_img);
+            i_img--;
+        }
+    } while (strcmp(comanda, "e") != 0);
+
+
+    delete_all(imagini, i_img, filtre, i_fil);
+
+
+    return 0;
+}
+
+
 int ***load_image(int N, int M, char *path) {
     // Alocam o noua matrice
     int ***m = (int ***)malloc(N * sizeof(int **));
     for (int i = 0; i < N; i++) {
         m[i] = (int **)malloc(M * sizeof(int *));
         for (int j = 0; j < M; j++) {
-            m[i][j] = (int *)malloc(3 * sizeof(int));
+            m[i][j] = (int *)malloc(COLOR_RANGE * sizeof(int));
         }
     }
 
@@ -34,9 +130,9 @@ void delete_image(imagine imagini[], int index, int i_img) {
         for (int j = 0; j < imagini[index].M; j++) {
             free(imagini[index].matrice[i][j]);
         }
-		free(imagini[index].matrice[i]);
+        free(imagini[index].matrice[i]);
     }
-	free(imagini[index].matrice);
+    free(imagini[index].matrice);
 
     for (int i = index; i < i_img; i++) {
         imagini[i] = imagini[i+1];
@@ -57,11 +153,11 @@ float **load_filter(int size) {
     return m;
 }
 
-void delete_filter(filtru filtre[10], int index, int i_fil) {
+void delete_filter(filtru filtre[SIZE_ARRAY], int index, int i_fil) {
     for (int i = 0; i < filtre[index].size; i++) {
-		free(filtre[index].matrice[i]);
+        free(filtre[index].matrice[i]);
     }
-	free(filtre[index].matrice);
+    free(filtre[index].matrice);
 
     for (int i = index; i < i_fil; i++) {
         filtre[i] = filtre[i+1];
@@ -78,113 +174,15 @@ void delete_all(imagine imagini[], int i_img, filtru filtre[], int i_fil) {
             for (int j = 0; j < imagini[index].M; j++) {
                 free(imagini[index].matrice[i][j]);
             }
-		    free(imagini[index].matrice[i]);
+            free(imagini[index].matrice[i]);
         }
-	    free(imagini[index].matrice);
+        free(imagini[index].matrice);
     }
 
     for (int index = 0; index < i_fil; index++) {
         for (int i = 0; i < filtre[index].size; i++) {
-		    free(filtre[index].matrice[i]);
+            free(filtre[index].matrice[i]);
         }
-	    free(filtre[index].matrice);
+        free(filtre[index].matrice);
     }
-}
-
-int main()
-{
-    int i_img = 0, i_fil = 0;
-    imagine imagini[10];
-    filtru filtre[10];
-    char comanda[2];
-
-    do {
-        scanf("%s", comanda);
-
-        if (strcmp(comanda, "l") == 0) {
-            int N, M;
-            char path[50];
-            scanf("%d %d %s", &N, &M, path);
-
-            imagini[i_img].matrice = load_image(N, M, path);
-            imagini[i_img].N = N;
-            imagini[i_img].M = M;
-            i_img++;
-            
-        } else if (strcmp(comanda, "s") == 0) {
-            int index;
-            char path[50];
-            scanf("%d %s", &index, path);
-
-            save(index, imagini, imagini[index].N, imagini[index].M, path);
-
-        } else if (strcmp(comanda, "ah") == 0) {
-            int index;
-            scanf("%d", &index);
-
-            imagini[index].matrice = flip_horizontal(imagini[index].matrice, imagini[index].N, imagini[index].M);
-
-        } else if (strcmp(comanda, "ar") == 0) {
-            int index, temp;
-            scanf("%d", &index);
-
-            imagini[index].matrice = rotate_left(imagini[index].matrice, imagini[index].N, imagini[index].M);
-            temp = imagini[index].N;
-            imagini[index].N = imagini[index].M;
-            imagini[index].M = temp;
-
-        } else if (strcmp(comanda, "ac") == 0) {
-            int index, x, y, h, w;
-            scanf("%d %d %d %d %d", &index, &x, &y, &w, &h);
-            imagini[index].matrice = crop(imagini[index].matrice, imagini[index].N, imagini[index].M, x, y, h, w);
-            imagini[index].N = h;
-            imagini[index].M = w;
-
-        } else if (strcmp(comanda, "ae") == 0) {
-            int index, rows, cols, R, G, B;
-            scanf("%d %d %d %d %d %d", &index, &rows, &cols, &R, &G, &B);
-            imagini[index].matrice = extend(imagini[index].matrice, imagini[index].N, imagini[index].M, rows, cols, R, G, B);
-            imagini[index].N += 2 * rows;
-            imagini[index].M += 2 * cols;
-
-        } else if (strcmp(comanda, "ap") == 0) {
-            int index_dst, index_src, x, y;
-            scanf("%d %d %d %d", &index_dst, &index_src, &x, &y);
-            imagini[index_dst].matrice = paste(imagini[index_dst].matrice, imagini[index_dst].N, imagini[index_dst].M, imagini[index_src].matrice, imagini[index_src].N, imagini[index_src].M, x, y);
-
-        } else if (strcmp(comanda, "cf") == 0) {
-            int size;
-            scanf("%d", &size);
-
-            filtre[i_fil].matrice = load_filter(size);
-            filtre[i_fil].size = size;
-            i_fil++;
-
-        } else if (strcmp(comanda, "af") == 0) {
-            int index_img, index_filter;
-            scanf("%d %d", &index_img, &index_filter);
-
-            imagini[index_img].matrice = apply_filter(imagini[index_img].matrice, imagini[index_img].N, imagini[index_img].M, filtre[index_filter].matrice, filtre[index_filter].size);
-        
-        } else if (strcmp(comanda, "df") == 0) {
-            int index;
-            scanf("%d", &index);
-
-            delete_filter(filtre, index, i_fil);
-            i_fil--;
-        } else if (strcmp(comanda, "di") == 0) {
-            int index;
-            scanf("%d", &index);
-
-            delete_image(imagini, index, i_img);
-            i_img--;
-        }
-
-    } while (strcmp(comanda, "e") != 0);
-
-    
-    delete_all(imagini, i_img, filtre, i_fil);
-	
-	
-    return 0;
 }
