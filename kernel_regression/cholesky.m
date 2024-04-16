4cf54d7facb7f8c9951ede618d3e8d8a61082365
deb <deb>
2024-04-16 21:41:16 +0300

    Task 2 in curs de rezolvare

--- kernel_regression/cholesky.m
+++ kernel_regression/cholesky.m
@@ -1,4 +1,21 @@
 function [L] = cholesky (A)
-    # TO DO: realizati descompunerea cholesky pentru o matrice
-    # positiv definita
+    [n,n] = size(A);
+    L = zeros(n);
+
+    % Pentru fiecare termen al diagonalei principale
+    for p = 1 : n
+        L(p,p) = sqrt(A(p,p));
+
+        % Pentru fiecare termen de sub cel al diagonalei
+        for i = p+1 : n
+            L(i,p) = A(i,p)/L(p,p);
+        end
+        % Pentru fiecare termen al minorului din dreapta jos al termenului
+        % actual de pe diagonala principala
+        for i = p+1 : n
+            for j = p+1 : n
+                A(i,j) = A(i,j) - L(i,p) * L(j,p);
+            end
+        end
+    end
 endfunction
--- kernel_regression/get_lower_inverse.m
+++ kernel_regression/get_lower_inverse.m
@@ -1,4 +1,16 @@
 function [P] = get_lower_inverse (L)
-  # TO DO: Determinati printr-o metoda matriceala neiterativa inversa
-  # unei matrici de tipul lower
+  [n, n] = size(L);
+  P = eye(n);
+
+  for j = 1 : n-1
+    for i = j+1 : n
+        m = L(i,j) / L(j,j);
+        P(i, :) = P(i,:) - m * P(j,:);
+    endfor
+  endfor
+
+  for i = 1 : n
+      P(i,:) = P(i,:) / L(i,i);
+
+  endfor
 endfunction
