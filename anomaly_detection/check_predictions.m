aa77e6bad0624e88e3e4d294a6f66f847e2abc8b
deb <deb>
2024-04-17 19:31:48 +0300

    Modificat sumele

--- anomaly_detection/check_predictions.m
+++ anomaly_detection/check_predictions.m
@@ -3,7 +3,22 @@
 % FN - numarul de anomalii, care nu au fost identificare
 % TP - numarul de anomalii identificate corect
 function [false_pozitives, false_negatives, true_pozitives] = check_predictions(predictions, truths)
-    false_pozitives = sum(predictions & ~truths);
-    false_negatives = sum(~predictions & truths);
-    true_pozitives = sum(predictions & truths);
+  [n, ~] = size(predictions);
+
+  % Initializeaza variabilele necesare
+  false_pozitives = 0;
+  false_negatives = 0;
+  true_pozitives = 0;
+
+  % Calculeaza numarul termenilor din cele 3 categorii
+  for i = 1 : n
+    if predictions(i) == 1 && truths(i) == 0
+      false_pozitives = false_pozitives + 1;
+    elseif predictions(i) == 0 && truths(i) == 1
+      false_negatives = false_negatives + 1;
+    elseif predictions(i) == 1 && truths(i) == 1
+      true_pozitives = true_pozitives + 1;
+    endif
+  endfor
+
 endfunction
