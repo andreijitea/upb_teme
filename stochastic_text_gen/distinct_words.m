440b8ff0223bbe0f7ee85c7a3fadee8b7eaf1606
deb <deb>
2024-04-19 15:17:00 +0300

    Task 3 aproape gata

--- stochastic_text_gen/distinct_words.m
+++ stochastic_text_gen/distinct_words.m
@@ -1,5 +1,6 @@
 
 function retval = distinct_words (tokens)
   % TODO: Find unique strings HINT: unique
-
+  retval = unique(tokens);
+  retval = sort(retval);
 endfunction
--- stochastic_text_gen/k_secv.m
+++ stochastic_text_gen/k_secv.m
@@ -2,4 +2,11 @@ function B = k_secv (A, k)
   % given an array [a b a b c] and k=2
   % should return [ab ba ab bc]
   % HINT: use strjoin 
+  m = length(A);
+  B = cell(m - k + 1, 1);
+
+  for i = 1 : m - k + 1
+    B(i) = strjoin(A(i : i + k - 1), ' ');
+  end
+  B(end) = [];
 endfunction
