7c6e6fe2938638a74431fa2810039d79ce9fd73b
dorel <dorel>
2024-05-14 15:13:15 +0300

    Jumatate de bonus gata

--- bonus/functional.asm
+++ bonus/functional.asm
@@ -7,13 +7,43 @@
 section .text
 global map
 global reduce
+;; void map(int64_t *destination_array, int64_t *source_array, int64_t array_size, int64_t(*f)(int64_t))
 map:
+    xor rax, rax
     ; look at these fancy registers
     push rbp
     mov rbp, rsp
+    ;; array_size = rdx
+    mov rdx, [rbp + 16]
+    ;; source_array = rsi
+    mov rsi, [rbp + 48]
+    ;; destination_array = rdi
+    mov rdi, [rbp + 56]
+    ;; f = rcx
 
-    ; sa-nceapa turneu'
+    ;; i = r8
+    mov r8, 0
+for_i:
+    ;; r9 = source_array[i]
+    mov r9, [rsi + 8 * r8]
 
+    ;; r9 = f(r9)
+    ;; Functia *f ia ca parametru rdi
+    mov rbx, rdi
+    mov rdi, r9
+    call rcx
+    mov r9, rax
+    mov rdi, rbx
+
+    ;; destination_array[i] = r9
+    mov qword [rdi + 8 * r8], r9
+    
+    ;; Incrementeaza for_i
+    inc r8
+    cmp r8, rdx
+    jl for_i
+
+end_map:
     leave
     ret
 
