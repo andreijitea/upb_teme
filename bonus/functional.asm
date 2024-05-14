0d6bceac387b40b8c6a6a9c41d96f72661d95bef
dorel <dorel>
2024-05-14 16:18:35 +0300

    Gata bonus

--- bonus/functional.asm
+++ bonus/functional.asm
@@ -8,6 +8,7 @@ section .text
 global map
 global reduce
 ;; void map(int64_t *destination_array, int64_t *source_array, int64_t array_size, int64_t(*f)(int64_t))
+;; int64_t map_func1(int64_t curr_elem);
 map:
     xor rax, rax
     ; look at these fancy registers
@@ -23,7 +24,7 @@ map:
 
     ;; i = r8
     mov r8, 0
-for_i:
+map_for_i:
     ;; r9 = source_array[i]
     mov r9, [rsi + 8 * r8]
 
@@ -37,11 +38,11 @@ for_i:
 
     ;; destination_array[i] = r9
     mov qword [rdi + 8 * r8], r9
-    
+
     ;; Incrementeaza for_i
     inc r8
     cmp r8, rdx
-    jl for_i
+    jl map_for_i
 
 end_map:
     leave
@@ -54,9 +55,40 @@ reduce:
     ; look at these fancy registers
     push rbp
     mov rbp, rsp
+    ;; n = rdx
+    mov rdx, [rbp + 16]
+    ;; src = rsi
+    mov rsi, [rbp + 48]
+    ;; f = r8
 
-    ; sa-nceapa festivalu'
+    ;; i = r9
+    mov r9, 0
 
+reduce_for_i:
+    ;; acc = f(acc, src[i])
+    ;; r10 = src[i]
+    mov r10, [rsi + 8 * r9]
+    ;; Functia *f ia ca parametrii rdi si rsi,
+    ;; il modifica pe rdx (salvez in stack)
+    mov r14, rdi
+    mov rdi, rcx
+    mov r15, rsi
+    mov rsi, r10
+    push rdx
+    call r8
+    pop rdx
+    mov rdi, r14
+    mov rsi, r15
+    mov rcx, rax
+
+
+    ;; Incrementeaza i
+    inc r9
+    cmp r9, rdx
+    jl reduce_for_i
+
+end_reduce:
+    mov rax, rcx
     leave
     ret
 
