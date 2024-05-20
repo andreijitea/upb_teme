133d4640fb79f5f709189c7262c0dc080c527d91
deb <deb>
2024-05-20 20:15:06 +0300

    Modificat task 3

--- recommendations/recommendations.m
+++ recommendations/recommendations.m
@@ -8,15 +8,17 @@ function recoms = recommendations(path, liked_theme, num_recoms, min_reviews, nu
 	% Calculeaza svd
 	[U, S, V] = svds(mat, num_features);
 
-	% Sorteaza temele dupa similaritati
+	% Transpune matricea de similaritati
+	V = V';
 	[m, n] = size(V);
-	aux = zeros(m, 1);
+	sim = zeros(n, 1);
 
-	for i = 1 : m
-		aux(i) = cosine_similarity(V(i, :)', V(liked_theme, :)');
+	for i = 1 : n
+		% Calculeaza similaritatea intre liked theme si tema curenta
+		sim(i) = cosine_similarity(V(:, i), V(:, liked_theme));
 	end
-	[~, sorted_ids] = sort(aux, 'descend');
+	% Sorteaza in ordine descrescatoare vectorul de similaritati
+	[~, ids] = sort(sim, 'descend');
 	% Pe prima pozitie este liked_theme
-	recoms = sorted_ids(2 : num_recoms + 1);
-	recoms = recoms';
+	recoms = ids(2 : num_recoms + 1)';
 end
